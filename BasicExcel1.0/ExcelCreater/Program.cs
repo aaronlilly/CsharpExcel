5039c9633066cb5e048e03b18a5b33ffa173bf4d
Aaron Lilly <aaronlilly84@gmail.com>
2020-09-28 15:10:45 -0400

    basic write to excel

---
+++ BasicExcel1.0/ExcelCreater/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML;
+using ClosedXML.Excel;
+
+namespace ExcelCreater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // start the excel machine.
+            var workbook = new XLWorkbook();
+            workbook.AddWorksheet("SheetName");
+            var ws = workbook.Worksheet("SheetName");
+
+            ws.Cell("A1").Value = "Hello World!";
+
+            var fileName = @"C:\test\abc.xlsx";
+            var fi = new FileInfo(fileName);
+            if (fi.Exists) File.Delete(fileName);
+
+            workbook.SaveAs(fileName);
+        }
+    }
+}
